e2cd63746a68dff6615ea5edd118861e118320be
boygl34 <56708291+boygl34@users.noreply.github.com>
2022-07-07 13:11:52 +0700

    all lexus
    Signed-off-by: boygl34 <56708291+boygl34@users.noreply.github.com>

--- js/timelineSCC.js
+++ js/timelineSCC.js
@@ -103,11 +103,11 @@ var timeline = new vis.Timeline(container, items,groups, options);
           if(r.TimeStartGJ){}else{
           var start =DoiNgayDangKy(r.ThoiGianHen)
           var end
-          if((r.NoiDungHen.toUpperCase()).indexOf("BD CẤP LỚN")>=0){end = new Date(1000 * 60 * 59 + (new Date(start)).valueOf())}
-          if((r.NoiDungHen.toUpperCase()).indexOf("LEXUS")>=0){end = new Date(1000 * 60 * 59 + (new Date(start)).valueOf())}
+         
           if(r.LoaiHinhSuaChua=="EM"||r.LoaiHinhSuaChua=="EM60"){  end=  new Date(1000 * 60 * 29 + (new Date(start)).valueOf())}
           if(r.LoaiHinhSuaChua=="SCC"||r.LoaiHinhSuaChua=="FIR"){  end=  new Date(1000 * 60 * 59 + (new Date(start)).valueOf())}
-          
+          if((r.NoiDungHen.toUpperCase()).indexOf("BD CẤP LỚN")>=0){end = new Date(1000 * 60 * 59 + (new Date(start)).valueOf())}
+          if((r.NoiDungHen.toUpperCase()).indexOf("LEXUS")>=0){end = new Date(1000 * 60 * 59 + (new Date(start)).valueOf())}
           var classnamehen ="blue"
             if(r.TDGapLeTan){classnamehen="orange"}
           items.add({
