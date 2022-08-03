8417f07c8815e84c42b78fbae1cd752b408c6d9d
boygl34 <56708291+boygl34@users.noreply.github.com>
2022-08-03 16:10:13 +0700

    ADD
    Signed-off-by: boygl34 <56708291+boygl34@users.noreply.github.com>

--- js/timelineBP.js
+++ js/timelineBP.js
@@ -54,9 +54,9 @@ var options = {
                 onUpdate: function (item) {
                         var BienSo = item.content.slice(0,item.content.indexOf("_"))
                         var CongDoan = item.content.slice(item.content.indexOf("_")+1,item.content.length-1)
-                        if(item.content.indexOf(kytu1)>0){CongDoan = item.content.slice(item.content.indexOf("_")+1,item.content.length-4)}
-                        if(item.content.indexOf(kytu2)>0){CongDoan = item.content.slice(item.content.indexOf("_")+1,item.content.length-4)}
-                        alert(CongDoan)
+                        if(item.content.indexOf(kytu1)>0){CongDoan = item.content.slice(item.content.indexOf("_")+1,item.content.length-3)}
+                        if(item.content.indexOf(kytu2)>0){CongDoan = item.content.slice(item.content.indexOf("_")+1,item.content.length-3)}
+                        //alert(CongDoan)
                         $("#buttonSCC").html('')
                         document.getElementById('FormDS').reset()
                         $('#ModalDongSon').modal('show')
